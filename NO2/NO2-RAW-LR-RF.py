d5d518baaa8ab56c37d9f6a343be09c3d7399af0
Yasaman Yazdani <167456732+yasyazdani@users.noreply.github.com>
2026-07-14 08:54:35 -0400

    Update NO2-RAW-LR-RF.py

--- NO2/NO2-RAW-LR-RF.py
+++ NO2/NO2-RAW-LR-RF.py
@@ -1,9 +1,7 @@
 # I changed the name of csv files from QuantAQ website to:
 # R-???.csv where ??? is the sensor ID and R is Raw data
 # F-???.cav where ??? is the sensor ID and F is Final data 
-# I didnt change the name of hdf files
-# Assuing  all files and this code are in the same directory
-# I am removing the negative voltage differnce in quantaq data
+# I removed the negative voltage differnce in quantaq data
 
 
 
